1ebfd48cac503d3a29a0f4cfd04a3df1c4d017af
Jason Barrie Morley <jbmorley@mac.com>
2022-08-19 21:52:31 +0100

    fix: Update the icon defaults

--- Iconic/Models/Document.swift
+++ Iconic/Models/Document.swift
@@ -22,14 +22,14 @@ import SwiftUI
 
 struct Document {
 
-    var topColor: Color = Color(NSColor(.black).lighter(by: 25))
-    var bottomColor: Color = .black
+    var topColor: Color = .pink
+    var bottomColor: Color = .purple
 
     var systemImage: String = "face.smiling"
     var symbolColor: Color = .white
-    var iconScale: CGFloat = 0.5
+    var iconScale: CGFloat = 0.8
 
-    var shadowOpacity: CGFloat = 0.5
-    var shadowHeight: CGFloat = 0.2
+    var shadowOpacity: CGFloat = 0.4
+    var shadowHeight: CGFloat = 0.3
 
 }
