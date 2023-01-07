77662909e4c68564d5e09f4e289c75911b27f7d6
Jason Morley <hello@jbmorley.co.uk>
2023-01-07 10:13:46 +0000

    Try to vertically center the symbol

--- Symbolic/Views/SymbolPicker.swift
+++ Symbolic/Views/SymbolPicker.swift
@@ -46,8 +46,10 @@ struct SymbolPicker: View {
                 isPresented = true
             } label: {
                 HStack {
+                    Spacer()
                     Image(systemName: systemImage.wrappedValue)
                         .imageScale(.large)
+                    Spacer()
                 }
                 .frame(width: 32, height: 32)
             }
