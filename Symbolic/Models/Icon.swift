902b0efe79a5d0f0b467ad13820ac9b5e8a8efd8
Jason Morley <hello@jbmorley.co.uk>
2023-01-29 13:26:53 +0000

    Remove divider and trailing whitespace from Swift license headers

--- Symbolic/Models/Icon.swift
+++ Symbolic/Models/Icon.swift
@@ -1,12 +1,12 @@
 // “Commons Clause” License Condition v1.0
-// 
+//
 // The Software is provided to you by the Licensor under the License, as defined
 // below, subject to the following condition.
-// 
+//
 // Without limiting other conditions in the License, the grant of rights under the
 // License will not include, and the License does not grant to you, the right to
 // Sell the Software.
-// 
+//
 // For purposes of the foregoing, “Sell” means practicing any or all of the rights
 // granted to you under the License to provide to third parties, for a fee or other
 // consideration (including without limitation fees for hosting or consulting/
@@ -14,31 +14,29 @@
 // derives, entirely or substantially, from the functionality of the Software. Any
 // license notice or attribution required by the License must also include this
 // Commons Clause License Condition notice.
-// 
+//
 // Software: Symbolic
 // License: BSD 3-Clause License
 // Licensor: InSeven Limited
-// 
-// ---
-// 
+//
 // BSD 3-Clause License
-// 
+//
 // Copyright (c) 2022-2023 InSeven Limited
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this
 //    list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its
 //    contributors may be used to endorse or promote products derived from
 //    this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -82,7 +80,7 @@ struct Icon: Identifiable, Codable {
     var shadowHeight: CGFloat = 0.3
 
     init() {
-        
+
     }
 
     init(from decoder: Decoder) throws {
