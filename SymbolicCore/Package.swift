00b07ce97d84097365066a571c4fd46a377f8006
Jason Morley <hello@jbmorley.co.uk>
2025-09-26 11:55:04 -1000

    Minimum platform version of 13

--- SymbolicCore/Package.swift
+++ SymbolicCore/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SymbolicCore",
+    platforms: [
+        .macOS(.v13),
+    ],
     products: [
         .library(
             name: "SymbolicCore",
