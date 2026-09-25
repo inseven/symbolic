0d741adfe16c6b85557c792e34f68b18a792d861
Jason Morley <hello@jbmorley.co.uk>
2026-09-26 07:33:05 +0900

    fix: 🧩 Update App Store app dependencies

--- apple/SymbolicCore/Package_App_Store.swift
+++ apple/SymbolicCore/Package_App_Store.swift
@@ -15,9 +15,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/sparkle-project/Sparkle", .upToNextMajor(from: "2.7.1")),
-        .package(url: "https://github.com/inseven/glitter.git", .upToNextMajor(from: "0.1.1")),
-        .package(url: "https://github.com/swhitty/SwiftDraw.git", .upToNextMajor(from: "0.9.6")),
+        .package(url: "https://github.com/sparkle-project/Sparkle", .upToNextMajor(from: "2.10.0")),
+        .package(url: "https://github.com/inseven/glitter.git", .upToNextMajor(from: "0.1.2")),
+        .package(url: "https://github.com/swhitty/SwiftDraw.git", .upToNextMajor(from: "0.29.0")),
     ],
     targets: [
         .target(
